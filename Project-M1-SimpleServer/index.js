cee6da17ed56d3da875292400ea844ea7e06d9b4
Abdulrahman Kabia <abdulrahmankabia@gmail.com>
2022-09-09 03:04:03 -0400

    Second Commit

--- Project-M1-SimpleServer/index.js
+++ Project-M1-SimpleServer/index.js
@@ -6,6 +6,24 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 
 /// Read data from file
+const tempLoan = require('./data/data.json');
+
+//Calculate the PV
+function Calculate(amount, interest, years) {
+    //const months = years*12
+    const months = years * 12;
+    //calculate i = interest/12
+    const interests = interest / 12;
+    //Calculate the PV
+    const PV = Math.round((amount / (interests)) * (1 - (1 / ((1 + ((interests))) ** (months)))) * 100) / 100;
+    return PV
+}
+
+//Add the PV into tempLoan
+tempLoan.forEach(function (item) {
+    item.PV = Calculate(item.loanAmount, item.interest, item.loanTermYears);
+})
+
 // Template
 const tempCourse = fs.readFileSync(
     `${__dirname}/data/data.json`,
@@ -31,7 +49,7 @@ const templateHTMLCourse = fs.readFileSync(
 //     return output;
 // }
 
- const dataObj = JSON.parse(tempCourse);// string to JavaScript Object JSON
+ const dataObj = tempLoan; // string to JavaScript Object JSON
 
 ////////////////////////////////
 //Create Server
@@ -42,21 +60,29 @@ const server = httpServer.createServer( (req, res) =>{// call back function
     // console.log(JSON.stringify(urlParameter.query));// convert to String
     // console.log(JSON.stringify(urlParameter.pathname));// convert to String
     const {query,pathname} = url.parse(req.url, true); // object distructors
-    if(query.id){// if there is query parameter named id read as string
+    if(query.id <=4){// if there is query parameter named id read as string
         // Courses page
-        if (pathname === '/' || pathname.toLowerCase() === '/courses') {
+        if (pathname === '/' || pathname.toLowerCase() === '/loan') {
             res.writeHead(200, {// Every thing ran successfully
                 'Content-type': 'text/html'
             });
-            const course = dataObj[Number(query.id)];// convert string to numeric value
-            const strCourseName = JSON.stringify(course);
-            const courseHTML = replaceTemplate(templateHTMLCourse, course);// function that will replace the course values in the HTML
+            const loan = dataObj[Number(query.id)];// convert string to numeric value
+            //const strCourseName = JSON.stringify(loan);
+            const loanHTML = replaceTemplate(templateHTMLLoan, loan);// function that will replace the course values in the HTML
             //   res.end(` We received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id}
             //   ${JSON.stringify(course)}// convert object back to string
             //   `)
-            res.end(courseHTML);
+            res.end(loanHTML);
         }
     }
+         
+    else if (query.id >= 5) {
+        res.writeHead(200, { // Server did not find what you were looking for
+            'Content-type': 'text/html'
+        });
+        res.end(`Please enter an id that is <=4 and >=0`)
+    }
+
     else{
             res.writeHead(404, {// Server did not find what you were looking for
                 'Content-type': 'text/html'
