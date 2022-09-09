7a345dc59d71869849858a0f465392d2cf70caac
Abdulrahman Kabia <abdulrahmankabia@gmail.com>
2022-09-09 10:49:03 -0400

    Fourth Commit

--- Project-M1-SimpleServer/index.js
+++ Project-M1-SimpleServer/index.js
@@ -28,14 +28,14 @@ tempLoan.forEach(function (item) {
 const tempCourse = fs.readFileSync(
     `${__dirname}/data/data.json`,
     'utf-8'
- );
+);
 
- /////////////////////////////////
+/////////////////////////////////
 // Template
 const templateHTMLLoan = fs.readFileSync(
     `${__dirname}/template/templateCourse.html`,
     'utf-8'
-  );
+);
 
 // //   function replaceTemplate(htmlStr, course){
 // const replaceTemplate = (htmlStr, course)=>{ // fat arrow function or lambda
@@ -49,50 +49,48 @@ const templateHTMLLoan = fs.readFileSync(
 //     return output;
 // }
 
- const dataObj = tempLoan; // string to JavaScript Object JSON
+const dataObj = tempLoan; // string to JavaScript Object JSON
 
 ////////////////////////////////
 //Create Server
 // const server = httpServer.createServer(function (req, res) {// call back function
-const server = httpServer.createServer( (req, res) =>{// call back function
+const server = httpServer.createServer((req, res) => { // call back function
 
     // const urlParameter = url.parse(req.url, true);
     // console.log(JSON.stringify(urlParameter.query));// convert to String
     // console.log(JSON.stringify(urlParameter.pathname));// convert to String
-    const {query,pathname} = url.parse(req.url, true); // object distructors
-    if(query.id <=4){// if there is query parameter named id read as string
+    const {
+        query,
+        pathname
+    } = url.parse(req.url, true); // object distructors
+    if (query.id <= 4) { // if there is query parameter named id read as string
         // Courses page
         if (pathname === '/' || pathname.toLowerCase() === '/loan') {
-            res.writeHead(200, {// Every thing ran successfully
+            res.writeHead(200, { // Every thing ran successfully
                 'Content-type': 'text/html'
             });
-            const loan = dataObj[Number(query.id)];// convert string to numeric value
+            const loan = dataObj[Number(query.id)]; // convert string to numeric value
             //const strCourseName = JSON.stringify(loan);
-            const loanHTML = replaceTemplate(templateHTMLLoan, loan);// function that will replace the course values in the HTML
+            const loanHTML = replaceTemplate(templateHTMLLoan, loan); // function that will replace the course values in the HTML
             //   res.end(` We received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id}
             //   ${JSON.stringify(course)}// convert object back to string
             //   `)
             res.end(loanHTML);
         }
-    }
-         
-    else if (query.id >= 5) {
+    } else if (query.id >= 5) {
         res.writeHead(200, { // Server did not find what you were looking for
             'Content-type': 'text/html'
         });
         res.end(`Please enter an id that is <=4 and >=0`)
+    } else {
+        res.writeHead(404, { // Server did not find what you were looking for
+            'Content-type': 'text/html'
+        });
+        res.end(`resource not found`)
     }
-
-    else{
-            res.writeHead(404, {// Server did not find what you were looking for
-                'Content-type': 'text/html'
-            });
-            res.end(`resource not found`)
-        }
-    });
-
-//Start Listening to requests
-server.listen(8000, 'localhost', ()=> {
-    console.log('Listening to requests on port 8000');
 });
 
+//Start Listening to requests
+server.listen(8000, 'localhost', () => {
+    console.log('Listening to requests on port 8000');
+});
